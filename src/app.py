bdfc8e61e504646d182f761dabd4b6fbc2e92d51
Robin <robin+git@rkslot.nl>
2022-09-20 15:28:30 +0200

    Update log format to more closely match gunicorn

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ import musicbrainz
 dictConfig({
     'version': 1,
     'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        'format': '[%(asctime)s] [%(process)d] [%(levelname)s] [%(module)s] %(message)s',
     }},
     'handlers': {'wsgi': {
         'class': 'logging.StreamHandler',
