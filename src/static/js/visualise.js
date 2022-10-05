4dc5faf98c8df0b8b8fcc9d3da5e1de63a83f815
Robin <robinslot@hotmail.nl>
2022-10-05 10:39:43 +0200

    Some visualiser improvements

--- src/static/js/visualise.js
+++ src/static/js/visualise.js
@@ -1,5 +1,5 @@
 const visualise = {
-    bars: 1024,
+    bars: 2048,
     analyser: null,
     dataArray: null,
     start: () => {
@@ -9,31 +9,35 @@ const visualise = {
         const analyser = audioContext.createAnalyser();
         audioSource.connect(analyser);
         audioSource.connect(audioContext.destination);
-        analyser.fftSize = visualise.bars;
         visualise.analyser = analyser;
         visualise.dataArray = new Uint8Array(analyser.frequencyBinCount);
         visualise.repeat();
     },
     repeat: () => {
-        const height = window.innerHeight / 4;
-        const width = window.innerWidth;
+        visualise.analyser.fftSize = visualise.bars;
         const canvas = document.getElementById('visualiser');
         canvas.style.zIndex = 2;
-        canvas.height = height;
-        canvas.width = width;
+        canvas.height = canvas.clientHeight;
+        canvas.width = canvas.clientWidth;
         const draw = canvas.getContext('2d');
         const bufferLength = visualise.analyser.frequencyBinCount;
-        const upperSkip = Math.round(0.35*bufferLength);
-        const barWidth = width / (bufferLength - upperSkip);
+        // const upperSkip = Math.round(0.35*bufferLength);
+        const skip = Math.round(0.15*bufferLength);
+        const barWidth = canvas.width / (bufferLength - 2.0*skip);
+
         let x = 0;
-        draw.clearRect(0, 0, height, width);
+        draw.clearRect(0, 0, canvas.height, canvas.width);
         visualise.analyser.getByteFrequencyData(visualise.dataArray);
-        for (let i = 0; i < (bufferLength - upperSkip); i++) {
+        let finalI = 0;
+        for (let i = skip; i < (bufferLength - skip); i++) {
             const barHeight = visualise.dataArray[i];
             draw.fillStyle = "white";
-            draw.fillRect(x, height - barHeight, barWidth, barHeight);
+            draw.fillRect(x, canvas.height - barHeight, barWidth + 1, barHeight);
             x += barWidth;
+            finalI = i;
         }
+
+        console.info(canvas.width, bufferLength, skip, barWidth, finalI);
 
         requestAnimationFrame(visualise.repeat);
     }
