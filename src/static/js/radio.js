5c0e5d1f097865f0acf1c6188dad4936b3677424
Robin <robinslot@hotmail.nl>
2022-12-18 23:34:17 +0100

    Sync radio using small slow-down or speed-up

--- src/static/js/radio.js
+++ src/static/js/radio.js
@@ -1,49 +1,56 @@
-setInterval(update, 2000);
 
-const minTimeCorrection = 1;
+const updateInterval = 2000;
+setInterval(update, updateInterval);
+setInterval(updateNext, 10_000);
 
 const state = {
     currentTrack: null,
     nextTrack: null,
-    audioElemNeedsChange: false,
 }
 
 async function update() {
     const currentResponse = await fetch('/radio_current');
     const currentJson = await currentResponse.json();
-    document.getElementById('current').innerText = currentJson.path;
-    const timezoneOffset = new Date().getTimezoneOffset() * 60;
-    const currentPos = (Date.now() - currentJson.start_time) / 1000 + timezoneOffset;
-    document.getElementById('current-pos').innerText = currentPos;
+
     if (state.currentTrack === null || state.currentTrack.path != currentJson.path) {
         state.currentTrack = currentJson;
-        state.audioElemNeedsChange = true;
+        console.info('replace audio elem')
+        replaceAudioElem();
+        return;
     }
 
+    const audioElem = document.getElementById('audio').childNodes[0];
+
+    if (audioElem === null) {
+        console.debug('audio elem null');
+        return;
+    }
+
+    document.getElementById('current').innerText = currentJson.path;
+    const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+    const currentPos = (Date.now() - currentJson.start_time) + timezoneOffset;
+    const offset = audioElem.currentTime*1000 - currentPos;
+    document.getElementById('current-pos').innerText = currentPos;
+
+    if (Math.abs(offset) > 1000) {
+        console.debug('large offset', offset, 'skip from', audioElem.currentTime, 'to', currentPos / 1000);
+        audioElem.currentTime = currentPos / 1000;
+        audioElem.playbackRate = 1;
+    } else {
+        const rate = 1 - offset / (10*updateInterval);
+        console.debug('small offset', offset, 'rate', rate);
+        audioElem.playbackRate = rate;
+    }
+};
+
+async function updateNext() {
     const nextResponse = await fetch('/radio_next');
     const nextJson = await nextResponse.json();
     document.getElementById('next').textContent = nextJson.path;
     document.getElementById('next-start').textContent = nextJson.start_time;
-
-    const audioElem = document.getElementById('audio').childNodes[0];
-    if (audioElem !== undefined) {
-        if (Math.abs(audioElem.currentTime - currentPos) > minTimeCorrection) {
-            console.log('time correction', audioElem.currentTime, currentPos);
-            audioElem.currentTime = currentPos;
-        }
-        audioElem.play();
-    }
-
-    replaceAudioElem();
 };
 
 async function replaceAudioElem() {
-    if (!state.audioElemNeedsChange) {
-        return;
-    }
-
-    state.audioElemNeedsChange = false;
-
     const audioElem = document.createElement('audio');
     console.log('replace children');
     const csrf = document.getElementById('csrf').textContent;
@@ -51,6 +58,7 @@ async function replaceAudioElem() {
     sourceElem.src = '/get_track?path=' + encodeURIComponent(state.currentTrack.path) + '&csrf=' + csrf;
     audioElem.appendChild(sourceElem);
     audioElem.controls = true;
+    audioElem.play();
 
     document.getElementById('audio').replaceChildren(audioElem);
 };
