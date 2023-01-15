a317d5f7a23d76d7216746c300020fcf92d4ed99
Robin <robinslot@hotmail.nl>
2023-01-15 17:38:20 +0100

    Allow connecting to database in read only mode

--- src/db.py
+++ src/db.py
@@ -10,26 +10,28 @@ db_path = Path(settings.data_path, 'music.db')
 log = logging.getLogger('app.db')
 
 
-def _connect(dbname: str) -> Connection:
-    conn = sqlite3.connect(Path(settings.data_path, dbname + '.db'),
-                           timeout=10.0)
+def _connect(db_name: str, read_only: bool) -> Connection:
+    db_uri = 'file:' + \
+          Path(settings.data_path, db_name + '.db').absolute().as_posix() + \
+          ('?mode=ro' if read_only else '')
+    conn = sqlite3.connect(db_uri, uri=True, timeout=10.0)
     conn.execute('PRAGMA foreign_keys = ON')
     conn.execute('PRAGMA journal_mode = WAL')
     return conn
 
 
-def connect() -> Connection:
+def connect(read_only: bool = False) -> Connection:
     """
     Create new SQLite database connection
     """
-    return _connect('music')
+    return _connect('music', read_only)
 
 
-def cache() -> Connection:
+def cache(read_only: bool = False) -> Connection:
     """
     Create new SQLite database connection to cache database
     """
-    return _connect('cache')
+    return _connect('cache', read_only)
 
 
 def create_tables():
