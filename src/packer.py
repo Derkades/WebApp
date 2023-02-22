dd6bab025c559543c7160c7fe2cc9f8dbdb5d60b
Robin Slot <robin+github@rkslot.nl>
2023-02-22 19:39:50 +0100

    Fix development packer

--- src/packer.py
+++ src/packer.py
@@ -7,6 +7,7 @@ def pack(path: Path) -> bytes:
     if not settings.dev:
         raise Exception('Python asset packer must only be used during development')
 
+    content = b''
     for path in sorted(path.iterdir()):
         with open(path, 'rb') as file:
             content += file.read()
