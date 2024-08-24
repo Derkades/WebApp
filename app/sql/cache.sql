b69fa3b3bfd1be51431a86b0c73a421ba4c55397
Derkades <robin+github@rkslot.nl>
2024-08-24 20:08:17 +0200

    Cache table in strict mode for new installations

--- app/sql/cache.sql
+++ app/sql/cache.sql
@@ -4,7 +4,7 @@ CREATE TABLE cache (
     key TEXT NOT NULL UNIQUE PRIMARY KEY,
     data BLOB NOT NULL,
     expire_time INTEGER NOT NULL
-);
+) STRICT; -- STRICT mode only for new databases since 2024-08-24, no migration exists for old databases as it would be too expensive
 
 CREATE INDEX idx_cache_expire_time ON cache(expire_time);
 
