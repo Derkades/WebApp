fbb5b5a28a4b12c8a33e8ec3b7c2271448e6dd22
Derkades <robin+github@rkslot.nl>
2024-08-24 14:10:56 +0200

    Simplify gain code

--- app/static/js/player/audiocontext.js
+++ app/static/js/player/audiocontext.js
@@ -3,7 +3,9 @@ class AudioContextManager {
     /** @type {AudioContext} */
     #audioContext;
     /** @type {GainNode} */
-    gain = null;
+    #gainNode = null;
+    /** @type {GainNode} */
+    #gainValue = null;
     /** @type {AnalyserNode} */
     analyser = null;
 
@@ -19,14 +21,39 @@ class AudioContextManager {
                 const source = this.#audioContext.createMediaElementSource(getAudioElement());
                 this.analyser = this.#audioContext.createAnalyser(); // used by visualiser
                 this.analyser.fftSize = this.fftSize;
-                this.gain = this.#audioContext.createGain();
+                this.#gainNode = this.#audioContext.createGain();
+                if (this.#gainValue) { // Gain was set while audio was still paused
+                    this.#gainNode.gain.value = this.#gainValue;
+                }
                 source.connect(this.analyser);
-                source.connect(this.gain);
-                this.gain.connect(this.#audioContext.destination)
+                source.connect(this.#gainNode);
+                this.#gainNode.connect(this.#audioContext.destination);
                 source.connect(this.#audioContext.destination);
             });
         });
     }
+
+    setGain(gain) {
+        // If gain node is available, we can immediately set the gain
+        // Otherwise, we only set the value and the 'play' event listener will set the gain
+
+        if (this.#gainNode) {
+            this.#gainNode.gain.value = gain;
+        } else {
+            this.#gainValue = gain;
+
+        }
+    }
 }
 
 const audioContextManager = new AudioContextManager();
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('settings-audio-gain').addEventListener('input', event => {
+        audioContextManager.setGain(event.target.value);
+    });
+});
+
+eventBus.subscribe(MusicEvent.SETTINGS_LOADED, () => {
+    audioContextManager.setGain(document.getElementById('settings-audio-gain').value);
+});
