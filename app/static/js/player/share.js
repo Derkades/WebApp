a64f9e97560d059247578f68ff59d7f0bb19fbea
Robin Slot <robin@rslot.nl>
2024-09-01 23:19:58 +0200

    Use Web Share API

--- app/static/js/player/share.js
+++ app/static/js/player/share.js
@@ -7,13 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     shareButton.addEventListener('click', (async function() {
-        if (!history.currentlyPlayingTrack) {
+        if (!queue.currentTrack || !queue.currentTrack.track) {
             // Music not loaded yet
             return;
         }
 
-        const response = await jsonPost('/share/create', {track: history.currentlyPlayingTrack.path});
+        const response = await jsonPost('/share/create', {track: queue.currentTrack.track.path});
         const json = await response.json()
-        window.open('/share/' + json.code, '_blank')
+        const absoluteShareUrl = new URL('/share/' + json.code, document.location).href;
+
+        const shareData = {url: absoluteShareUrl};
+
+        if (navigator.canShare) {
+            if (navigator.canShare(shareData)) {
+                navigator.share(shareData);
+                return;
+            } else {
+                console.warn('share: canShare = false');
+            }
+        } else {
+            console.warn('share: Share API is not available');
+        }
+
+        window.open(absoluteShareUrl, '_blank');
     }));
 });
