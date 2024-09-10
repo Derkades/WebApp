8d1643d4c75e8473b7eb86c5ba0a79fe68892195
Robin Slot <robin@rslot.nl>
2024-09-10 13:53:35 +0200

    Set volume using gain node, bypassing visualiser

--- app/static/js/player/audiocontext.js
+++ app/static/js/player/audiocontext.js
@@ -4,7 +4,7 @@ class AudioContextManager {
     #audioContext;
     /** @type {GainNode} */
     #gainNode = null;
-    /** @type {GainNode} */
+    /** @type {number} */
     #gainValue = null;
     /** @type {AnalyserNode} */
     analyser = null;
@@ -22,9 +22,7 @@ class AudioContextManager {
                 this.analyser = this.#audioContext.createAnalyser(); // used by visualiser
                 this.analyser.fftSize = this.fftSize;
                 this.#gainNode = this.#audioContext.createGain();
-                if (this.#gainValue) { // Gain was set while audio was still paused
-                    this.#gainNode.gain.value = this.#gainValue;
-                }
+                this.applyGain(); // If gain was set while audio was still paused
                 source.connect(this.analyser);
                 source.connect(this.#gainNode);
                 this.#gainNode.connect(this.#audioContext.destination);
@@ -32,27 +30,71 @@ class AudioContextManager {
         });
     }
 
-    setGain(gain) {
+    applyGain() {
         // If gain node is available, we can immediately set the gain
-        // Otherwise, we only set the value and the 'play' event listener will set the gain
-
-        if (this.#gainNode) {
-            this.#gainNode.gain.value = gain;
-        } else {
-            this.#gainValue = gain;
-
+        // Otherwise, the 'play' event listener will call this method again
+        if (!this.#gainNode) {
+            console.debug('autocontext: gainNode not available yet');
+            return;
         }
+        const gain = this.#gainValue ? this.#gainValue : 1;
+        const volume = getTransformedVolume(document.getElementById('settings-volume').value);
+        console.debug('audiocontext: set gain:', gain, volume, gain * volume);
+        // exponential function cannot handle 0 value, so clamp to tiny minimum value instead
+        this.#gainNode.gain.exponentialRampToValueAtTime(Math.max(gain * volume, 0.0001), this.#audioContext.currentTime + 0.1);
+    }
+
+    setGain(gain) {
+        this.#gainValue = gain;
+        this.applyGain();
     }
 }
 
 const audioContextManager = new AudioContextManager();
 
+function getTransformedVolume(volumeZeroToHundred) {
+    // https://www.dr-lex.be/info-stuff/volumecontrols.html
+    return Math.pow(volumeZeroToHundred / 100, 3);
+}
+
+function onVolumeChange() {
+    const slider = document.getElementById('settings-volume');
+    const volume = slider.value;
+
+    slider.classList.remove('input-volume-high', 'input-volume-medium', 'input-volume-low');
+    if (volume > 60) {
+        slider.classList.add('input-volume-high');
+    } else if (volume > 30) {
+        slider.classList.add('input-volume-medium');
+    } else {
+        slider.classList.add('input-volume-low');
+    }
+
+    audioContextManager.applyGain();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    // Respond to gain changes
     document.getElementById('settings-audio-gain').addEventListener('input', event => {
         audioContextManager.setGain(event.target.value);
     });
+
+    // Respond to volume button changes
+    document.getElementById('settings-volume').addEventListener('input', () => onVolumeChange());
+
+    // Scroll to change volume
+    const volume = document.getElementById('settings-volume');
+    volume.addEventListener('wheel', event => {
+        volume.value = parseInt(volume.value) + (event.deltaY < 0 ? 2 : -2);
+        onVolumeChange();
+    }, {passive: true});
+
+    // Audio element should always be playing at max volume
+    // Volume is set using GainNode
+    getAudioElement().volume = 1;
 });
 
 eventBus.subscribe(MusicEvent.SETTINGS_LOADED, () => {
     audioContextManager.setGain(document.getElementById('settings-audio-gain').value);
+    onVolumeChange();
 });
