77726e31760c1174b8b3abc8e0a22d49f96e4578
Robin Slot <robin@rslot.nl>
2024-10-21 23:39:30 +0200

    Rewrite to be more resource friendly

--- raphson_mp/static/js/player/lyrics.js
+++ raphson_mp/static/js/player/lyrics.js
@@ -2,27 +2,48 @@ document.addEventListener('DOMContentLoaded', () => {
     const audioElem = getAudioElement();
     const lyricsElem = document.getElementById('lyrics-text');
 
-    // Handling time-synced lyrics
-    audioElem.addEventListener('timeupdate', () => {
-        if (!queue.currentTrack){
-            console.warn('lyrics: no current track');
-        }
+    let lastLine = null;
 
+    function updateLyrics() {
         const lyrics = queue.currentTrack.lyrics;
-        if (lyrics instanceof TimeSyncedLyrics) {
-            const currentLine = lyrics.currentLine(audioElem.currentTime);
+        const currentLine = lyrics.currentLine(audioElem.currentTime);
 
-            // Show current line, with context
-            const context = 3;
-            let lyricsText = "";
-            for (let i = currentLine - context; i <= currentLine + context; i++) {
-                if (i < 0 || i >= lyrics.text.length) {
-                    continue;
-                }
-                lyricsText += lyrics.text[i].text + '\n';
-            }
-
-            lyricsElem.textContent = lyricsText;
+        if (currentLine == lastLine) {
+            // Still the same line, no need to cause expensive DOM update.
+            return;
         }
-    });
+
+        lastLine = currentLine;
+
+        // Show current line, with context
+        const context = 3;
+        let lyricsText = "";
+        for (let i = currentLine - context; i <= currentLine + context; i++) {
+            if (i < 0 || i >= lyrics.text.length) {
+                continue;
+            }
+            lyricsText += lyrics.text[i].text + '\n';
+        }
+
+        lyricsElem.textContent = lyricsText;
+    }
+
+    function registerListener() {
+        if (document.visibilityState == 'visible'
+            && queue.currentTrack
+            && queue.currentTrack.lyrics
+            && queue.currentTrack.lyrics instanceof TimeSyncedLyrics) {
+            console.debug('lyrics: registered timeupdate listener');
+            audioElem.removeEventListener('timeupdate', updateLyrics); // remove it in case it is already registered
+            audioElem.addEventListener('timeupdate', updateLyrics);
+            return;
+        } else {
+            console.debug('lyrics: unregistered timeupdate listener');
+            audioElem.removeEventListener('timeupdate', updateLyrics);
+        }
+    }
+
+    eventBus.subscribe(MusicEvent.TRACK_CHANGE, registerListener);
+
+    document.addEventListener('visibilitychange', registerListener);
 });
