e726ea13a0e593b1e39915afcd2225552018974a
Robin Slot <robin@rslot.nl>
2024-10-21 22:41:50 +0200

    Minor code improvements

--- raphson_mp/static/js/player/lyrics.js
+++ raphson_mp/static/js/player/lyrics.js
@@ -10,19 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const lyrics = queue.currentTrack.lyrics;
         if (lyrics instanceof TimeSyncedLyrics) {
-            lyricsElem.style.fontSize = '1.2em';
             const currentLine = lyrics.currentLine(audioElem.currentTime);
-            console.log(currentLine);
-            lyricsElem.textContent = "";
 
             // Show current line, with context
             const context = 3;
+            let lyricsText = "";
             for (let i = currentLine - context; i <= currentLine + context; i++) {
                 if (i < 0 || i >= lyrics.text.length) {
                     continue;
                 }
-                lyricsElem.textContent += lyrics.text[i].text + '\n';
+                lyricsText += lyrics.text[i].text + '\n';
             }
+
+            lyricsElem.textContent = lyricsText;
         }
     });
 });
