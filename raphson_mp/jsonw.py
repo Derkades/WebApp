042716254bcfbe566de5ba137fbd82d96fa11027
Robin Slot <robin@rslot.nl>
2024-10-22 09:42:25 +0200

    Flask also accepts integer timestamps

--- raphson_mp/jsonw.py
+++ raphson_mp/jsonw.py
@@ -10,7 +10,7 @@ def to_json(obj: Any) -> str:
     return json.dumps(obj, allow_nan=False, separators=(',', ':'))
 
 
-def json_response(obj: Any, last_modified: Optional[datetime] = None) -> Response:
+def json_response(obj: Any, last_modified: Optional[datetime|int] = None) -> Response:
     """Convert object to json, and return as Flask response"""
     response = Response(to_json(obj), content_type='application/json')
     if last_modified:
