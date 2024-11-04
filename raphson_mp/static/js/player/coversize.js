a5c1cbf79a045c2f468a46c7718ff0490a504096
Robin Slot <robin@rslot.nl>
2024-11-04 17:10:20 +0100

    Make album cover as large as it can be

---
+++ raphson_mp/static/js/player/coversize.js
@@ -0,0 +1,33 @@
+// Ensures combined height of album cover and lyrics box never exceed 100vh
+
+document.addEventListener('DOMContentLoaded', () => {
+    const lyricsBox = document.getElementById('lyrics-box');
+    const coverBox = document.getElementById('album-cover-box');
+    let lastHeight = 0;
+
+    function setMaxHeight(value) {
+        coverBox.style.maxHeight = value;
+        coverBox.style.maxWidth = value;
+        lyricsBox.style.maxWidth = value;
+    }
+
+    function resizeCover() {
+        if (lyricsBox.clientHeight == lastHeight) {
+            return;
+        }
+        lastHeight = lyricsBox.clientHeight;
+
+        if (lyricsBox.classList.contains('hidden')) {
+            // No lyrics
+            setMaxHeight(`calc(100vh - 2*var(--gap)`);
+            return;
+        }
+
+        setMaxHeight(`calc(100vh - 3*var(--gap) - ${lyricsBox.clientHeight}px)`);
+
+        console.debug('coversize: height changed:', lyricsBox.clientHeight);
+    }
+
+    const resizeObserver = new ResizeObserver(resizeCover);
+    resizeObserver.observe(lyricsBox);
+});
