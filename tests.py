2b27f35eac963a0dde415b3717d9f7469a5bfe35
Robin Slot <robin@rslot.nl>
2024-11-04 22:42:27 +0100

    Add some tests

---
+++ tests.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import random
+import unittest
+import secrets
+
+from flask.testing import FlaskClient
+from raphson_mp import main, settings
+
+import tracemalloc
+
+tracemalloc.start()
+
+
+TEST_USERNAME = 'robin'
+TEST_PASSWORD = '1234'
+
+
+class TestFlask(unittest.TestCase):
+    client: FlaskClient
+
+    def setUp(self):
+        settings.data_dir = Path('./data').resolve()
+        settings.music_dir = Path('./music').resolve()
+        app = main.get_app(0, False)
+        app.testing = True
+        self.client = app.test_client()
+        response = self.client.post('/auth/login', data={'username': TEST_USERNAME, 'password': TEST_PASSWORD})
+        assert response.status_code == 303
+
+    def test_login_fail(self):
+        response = self.client.post('/auth/login', json={'username': TEST_USERNAME, 'password': secrets.token_urlsafe(random.randint(1, 100))})
+        assert response.status_code == 403, response.status + response.text
+
+    def test_login_json(self):
+        response = self.client.post('/auth/login', json={'username': TEST_USERNAME, 'password': TEST_PASSWORD})
+        assert response.status_code == 200
+        self.token: str = response.json['token']
+        assert len(self.token) > 10
+
+    def test_playlist_list(self):
+        response = self.client.get('/playlist/list')
+        playlists = response.json
+        assert type(playlists) == list
+        playlist = playlists[0]
+        assert type(playlist['name']) == str
+        assert type(playlist['track_count']) == int
+        assert type(playlist['favorite']) == bool
+        assert type(playlist['write']) == bool
+
+if __name__ == '__main__':
+    unittest.main()
