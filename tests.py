cd5505b067c04d4dc2f3a76bc7feeed4ec630e08
Robin Slot <robin@rslot.nl>
2024-11-06 21:34:02 +0100

    Add tests

--- tests.py
+++ tests.py
@@ -1,12 +1,17 @@
+import os
 import random
 import secrets
+from tempfile import TemporaryDirectory
 import tracemalloc
+from typing import Any, cast, override
 import unittest
 from pathlib import Path
 
 from flask.testing import FlaskClient
 
-from raphson_mp import main, settings
+from raphson_mp import main, packer, reddit, settings
+from raphson_mp.lyrics import AZLyricsFetcher, GeniusFetcher, LrcLibFetcher, PlainLyrics, TimeSyncedLyrics
+from raphson_mp.spotify import SpotifyClient
 
 tracemalloc.start()
 
@@ -14,13 +19,15 @@ tracemalloc.start()
 TEST_USERNAME = 'robin'
 TEST_PASSWORD = '1234'
 
+settings.data_dir = Path('./data').resolve()
+settings.music_dir = Path('./music').resolve()
+
 
 class TestFlask(unittest.TestCase):
-    client: FlaskClient
+    client: FlaskClient  # pyright: ignore[reportUninitializedInstanceVariable]
 
+    @override
     def setUp(self):
-        settings.data_dir = Path('./data').resolve()
-        settings.music_dir = Path('./music').resolve()
         app = main.get_app(0, False)
         app.testing = True
         self.client = app.test_client()
@@ -34,18 +41,63 @@ class TestFlask(unittest.TestCase):
     def test_login_json(self):
         response = self.client.post('/auth/login', json={'username': TEST_USERNAME, 'password': TEST_PASSWORD})
         assert response.status_code == 200
-        self.token: str = response.json['token']
-        assert len(self.token) > 10
+        token = cast(str, response.json['token'])
+        assert len(token) > 10
 
     def test_playlist_list(self):
         response = self.client.get('/playlist/list')
-        playlists = response.json
-        assert type(playlists) == list
+        playlists = cast(Any, response.json)
         playlist = playlists[0]
         assert type(playlist['name']) == str
         assert type(playlist['track_count']) == int
         assert type(playlist['favorite']) == bool
         assert type(playlist['write']) == bool
+
+
+class TestLyrics(unittest.TestCase):
+    client: SpotifyClient  # pyright: ignore[reportUninitializedInstanceVariable]
+
+    def test_lrclib(self):
+        # full length cd version
+        lyrics = LrcLibFetcher().find('Strong', 'London Grammar', 'If You Wait', 276)
+        assert isinstance(lyrics, TimeSyncedLyrics), lyrics
+        assert lyrics.text[0].text == 'Excuse me for a while', lyrics.text[0]
+        assert lyrics.text[0].start_time == 43.56, lyrics.text[0]
+
+        # music video version
+        lyrics = LrcLibFetcher().find('Strong', 'London Grammar', 'If You Wait', 242)
+        assert isinstance(lyrics, TimeSyncedLyrics), lyrics
+        assert lyrics.text[0].text == 'Excuse me for a while', lyrics.text[0]
+        assert lyrics.text[0].start_time == 14.6, lyrics.text[0]
+
+    def test_azlyrics(self):
+        lyrics = AZLyricsFetcher().find('Starburster', 'Fontaines D.C.', None, None)
+        assert isinstance(lyrics, PlainLyrics)
+        assert "I wanna see you alone, I wanna sharp the stone" in lyrics.text, lyrics.text
+
+    def test_genius(self):
+        lyrics = GeniusFetcher().find('Give Me One Reason', 'Tracy Chapman', None, None)
+        assert isinstance(lyrics, PlainLyrics)
+        assert "You know that I called you, I called too many times" in lyrics.text, lyrics.text
+
+
+class TestPacker(unittest.TestCase):
+    def test_packer(self):
+        with TemporaryDirectory() as tempdir:
+            a = os.urandom(random.randint(0, 1000))
+            b = os.urandom(random.randint(0, 1000))
+            Path(tempdir, 'a').write_bytes(a)
+            Path(tempdir, 'b').write_bytes(b)
+            result = packer.pack(Path(tempdir))
+            assert result == a + b
+
+
+class TestReddit(unittest.TestCase):
+    def test_search(self):
+        image_url = reddit.search('test')
+        assert image_url
+        assert image_url.startswith('https://'), image_url
+
 
 if __name__ == '__main__':
     unittest.main()
