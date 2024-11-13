f450a985c2a507abfcab938e790263b528be84b8
Robin Slot <robin@rslot.nl>
2024-11-13 13:58:49 +0100

    Fix typo in install script

--- raphson_mp/static/js/install.js
+++ raphson_mp/static/js/install.js
@@ -2,12 +2,11 @@ let deferredPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevents the default mini-infobar or install dialog from appearing on mobile
     e.preventDefault();
-    // Save the event because you'll need to trigger it later.
+
     deferredPrompt = e;
 
     const no = document.getElementById('no-support');
-    const yes = document.getElement
-    ById('yes-support');
+    const yes = document.getElementById('yes-support');
     yes.classList.remove('hidden');
     no.classList.add('hidden');
 
